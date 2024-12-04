7ca23b8ca2f24aeb97862c25b78f0a8f6416ff63
jawednur <133543592+jawednur@users.noreply.github.com>
2024-12-04 10:00:06 -0500

    focused on styling and trying to fix cache issues with loading in css class after page is loaded. Specifically troubleshooted multi-loading of pop- ups and styling.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -8,63 +8,160 @@
 
 // List Collection Items (GET /collections/:collection_id/items)
 
+document.addEventListener('DOMContentLoaded', () => {
+	const changeImagesBtn = document.getElementById('changeImages');
+
+	changeImagesBtn.addEventListener('click', () => {
+
+		 const allImages = document.querySelectorAll('img');
+
+
+		 const newImageUrl = "./images/IMG_3375.jpeg";
+
+		 // Asked copiloit how to change each image's source to the new image
+		 allImages.forEach(img => {
+			  img.src = newImageUrl;
+			  // Copilot suggested to add this line to make the images fit the container
+			  img.style.objectFit = 'cover';
+		 });
+	});
+});
 function matchIDToAuthor(author) {
 	return fetch('js/teamMembers.json')
-		 .then(response => response.json())
-		 .then(data => {
-			  for (let i = 0; i < data.items.length; i++) {
-					if (author == data.items[i].id) {
-						 return data.items[i].fieldData.name;
-					}
-		 }});
+		.then(response => response.json())
+		.then(data => {
+			for (let i = 0; i < data.items.length; i++) {
+				if (author == data.items[i].id) {
+					return data.items[i].fieldData.name;
+				}
+			}
+		});
 }
 const mainFrame = document.querySelectorAll(".main-frame-items");
+const navFrame = document.querySelectorAll(".main-container")
+
+
 function featuredArticles() {
 	if (document.activeElement.attributes[0].nodeValue == "index") {
-		 fetch('js/articles.json')
-			  .then(response => response.json())
-			  .then(async (data) => {  // Make this callback async
-					for (let i = 0; i < 20; i++) {
-						 if (data.items[i].fieldData.featured == true) {
-							  const name = data.items[i].fieldData.name;
-							  const summary = data.items[i].fieldData.postsummary;
-							  const image = data.items[i].fieldData['website-image'].url;
-							  const photo = `<img src="${encodeURI(image)}" class="article-image">`;
+		fetch('js/articles.json')
+			.then(response => response.json())
+			.then(async (data) => {  // Make this callback async
+				for (let i = 0; i < data.items.length; i++) {
+					if (data.items[i].fieldData.featured == true) {
+						const name = data.items[i].fieldData.name;
+						const summary = data.items[i].fieldData.postsummary;
+						const image = data.items[i].fieldData['website-image'].url;
+						const photo = `<img src="${encodeURI(image)}" class="article-image">`;
 
-							  // Wait for the author name to be fetched
-							  const author = await matchIDToAuthor(data.items[i].fieldData.author);
 
-							  const article = `<a class="target"  href="./Articles.html">
+						const article = `
 									<section class="article-box">
 										 ${photo}
 										 <div class="article-text">
-											  <h2>${name}</h2>
+											  <h2 class="name">${name}</h2>
 											  <p>${summary}</p>
-											  <h3>${author}</h3>
+
+											  <div class = "featured">
+											  <h3>Featured</h3>
 										 </div>
 									</section>
-							  </a>`;
-							  mainFrame[0].insertAdjacentHTML('beforeend', article);
-						 }
+							  `;
+
+						mainFrame[0].insertAdjacentHTML('beforeend', article);
+
 					}
-			  });
+
+					addClickListenersToArticleTitles();
+				}
+			}
+
+			)
 	}
 }
+const articleBox = document.querySelectorAll(".article-box");
 
-/*if (author == teamData.items[i].id) {
-	return authorData.items[i].fieldData.author = teamData.items[i].id;
+let backButtonAdded = false; // asked copiloit how I can check to do a single run if a class exists
+
+function backB() {
+    if (!backButtonAdded) {
+        const backButton = "<button class='back-button' onclick='history.back()'>Back</button>";
+        navFrame[0].attributes[0].ownerElement.childNodes[1].insertAdjacentHTML('afterbegin', backButton);
+        backButtonAdded = true; // Set the flag to true after adding the button
+    }
 }
-console.log('end of loop');
+function backBAltTwo (){
+    if (!backButtonAdded) {
+        const backButton = "<button class='back-button' onclick='history.back()'>Back</button>";
+        mainFrame[0].children[0].childNodes[3].insertAdjacentHTML('beforeend', backButton);
+        backButtonAdded = false; // Set the flag to true after adding the button
+    }
 }
+function backBAlt (){
+	if (!backButtonAdded) {
+		 const backButton = "<button class='back-button' onclick='history.back()'>Back</button>";
+		 navFrame[0].attributes[0].ownerElement.childNodes[1].insertAdjacentHTML('beforeend', backButton);
+		 backButtonAdded = true; // Set the flag to true after adding the button
+	}
 }
-*/
+function addClickListenersToArticleTitles() {
+	const articleTitles = document.querySelectorAll('.article-box');
+	articleTitles.forEach(title => {
+		title.addEventListener('click', (event) => {
+			const clickedTitle = title.querySelector('.name').textContent;
+			displayArticleContent(clickedTitle);
+			const page = document.body.getAttribute('data-page');
+			if (page === 'Articles') {
+				 backBAlt();
+			} else if (page === 'index') {
+				 backB();
+
+			}
+		})
+
+	});
+};
+
+function displayArticleContent(titleToMatch) {
+	fetch('js/articles.json')
+		.then(response => response.json())
+		.then(async (data) => {
+			const article = data.items.find(item =>
+				item.fieldData.name === titleToMatch
+			);
+
+			if (article) {
+				const author = await matchIDToAuthor(article.fieldData.author);
+				const words = article.fieldData['article-text'];
+
+				const content = `
+                    <article class="full-article">
+                        <h1>${article.fieldData.name}</h1>
+                        <img src="${encodeURI(article.fieldData['website-image'].url)}"
+                             class="article-full-image">
+                        <div class="article-content">
+                            <h2>By ${author}</h2>
+                            <div class="article-body">
+                                ${words}
+                            </div>
+                        </div>
+                    </article>
+                `;
+
+				document.querySelector('.main-frame-items').innerHTML = content; // asked copilot to explain how to overwrite on the page
+			}
+		});
+}
 
 if (document.activeElement.attributes[0].nodeValue == "index") {
 	featuredArticles();
 }
 
+
 if (document.activeElement.attributes[0].nodeValue == "Articles") {
 	addArticles();
+	backBAltTwo();
+
+
 }
 function addArticles() {
 	fetch('js/articles.json')
@@ -81,69 +178,131 @@ function addArticles() {
 			<article class="article-page">
 				${photo}
 				<div class="article-text">
-					<h2>${name}</h2>
+					<h2 class="name">${name}</h2>
 					<h3>${author}</h3>
 					<p> ${summary}</p>
 				</div>`;
 				mainFrame[0].insertAdjacentHTML('beforeend', [article])
 			}
+			addClickListenersToArticleTitles();
 		})
 }
 
 if (document.activeElement.attributes[0].nodeValue == "Article Page") {
 	addArticlePage();
 }
-
-
-/*CODE FOR THE AUTHOR SEARCH TO BE DONE IN EACH ARTICLE
-let author = data.items[i].fieldData.author;
-fetch('js/team members.json')
-.then(response => response.json())
-.then((data)=> {
-	for (let j = 0; j <= data.items.length; j++){
-		if (author == data.items[j].id) {
-			author = data.items[j].name;
-			console.log(author);
-			return;
-		} else {
-			author = "Anonymous";
-		}
+document.addEventListener('DOMContentLoaded', () => {
+	if (document.body.getAttribute('data-page') === "Teams") {
+		 loadTeamMembers();
 	}
-})
+	const showFeaturedButton = document.getElementById('show-featured');
+	if (showFeaturedButton) {
+		 showFeaturedButton.addEventListener('click', showFeaturedArticles);
+	}
+});
 
-/*fetch('js/articles.json')
-.then(response => response.json())
-.then((data) => {
-for (let i = 0; i < 20; i++) {
-if (data.items[i].fieldData.featured ==true) {
- const name = data.items[i].fieldData.name;
- const summary = data.items[i].fieldData.postsummary;
- const image = data.items[i].fieldData['website-image'].url;
- const photo = `<img src="${encodeURI(image)}" class="article-image">`;
- console.log(photo);
- const article = `<a class="target" target="_blank" href="./Articles.html">
-	<section class="article-box">
-	  ${photo}
-	  <div class="article-text">
-		 <h2>${name}</h2>
-		 <p>${summary}</p>
-	  </div>
-	</section>
- </a>`;
- mainFrame[0].insertAdjacentHTML('beforeend', article);
+async function showFeaturedArticles() {
+	const response = await fetch('js/articles.json');
+	const data = await response.json();
+
+	// asked chat how to clear the existing articles
+	document.querySelector('.main-frame-items').innerHTML = '';
+
+	// Filter and display featured articles
+	for (const item of data.items) {
+		 if (item.fieldData.featured) {
+			  const name = item.fieldData.name;
+			  const summary = item.fieldData.postsummary;
+			  const image = item.fieldData['website-image'].url;
+			  const photo = `<img src="${encodeURI(image)}" class="article-image">`;
+			  const author = await matchIDToAuthor(item.fieldData.author);
+
+			  const article = `
+					<div class="featured">
+						 <h3>Featured</h3>
+					</div>
+					<div class="article-box">
+						 <article class="article-page">
+							  ${photo}
+							  <div class="article-text">
+									<h2 class="name">${name}</h2>
+									<h3>${author}</h3>
+									<p>${summary}</p>
+							  </div>
+						 </article>
+					</div>
+			  `;
+
+			  document.querySelector('.main-frame-items').insertAdjacentHTML('beforeend', article);
+		 }
+	}
+
+	backBAlt();
+	addClickListenersToArticleTitles();
 }
+
+async function loadTeamMembers() {
+	const response = await fetch('js/teamMembers.json');
+	const data = await response.json();
+
+	for (const member of data.items) {
+		 const authorArticles = await getAuthorArticles(member.id);
+
+		 const teamMember = `
+			  <div class="team-member-box">
+					<div class="member-info">
+						 <h2 class="member-name" data-author-id="${member.id}">${member.fieldData.name}</h2>
+					</div>
+					<div class="member-articles">
+						 <h4>Articles by ${member.fieldData.name}</h4>
+						 <div class="article-list">
+							  ${authorArticles}
+						 </div>
+					</div>
+			  </div>
+		 `;
+		 document.querySelector('.main-frame-items').insertAdjacentHTML('beforeend', teamMember);
+	}
+	addClickListenersToMemberArticles();
 }
 
-}) */
+function getAuthorArticles(authorId) {
+	return fetch('js/articles.json')
+		.then(response => response.json())
+		.then(data => {
+			let articleHTML = '';
+			const authorArticles = data.items.filter(item =>
+				item.fieldData.author === authorId
+			);
+
+			authorArticles.forEach(article => {
+				articleHTML += `
+						 <div class="author-article">
+							  <img src="${encodeURI(article.fieldData['website-image'].url)}"
+									 class="article-thumbnail">
+							  <div class="article-preview">
+									<h2 class="name">${article.fieldData.name}</h2>
+									<p>${article.fieldData.postsummary}</p>
+							  </div>
+						 </div>
+					`;
+			});
+
+			return articleHTML || '<p>No articles published yet.</p>';
+		});
+}
+
+function addClickListenersToMemberArticles() {
+	const articleTitles = document.querySelectorAll('.member-articles .name');
+	articleTitles.forEach(title => {
+		title.addEventListener('click', (event) => {
+			const clickedTitle = event.target.textContent;
+			displayArticleContent(clickedTitle);
+		});
+	});
+}
 
 
-
-
-//function addArticle(article){
-//article = webflowArticleHead;
-
-
-//console.log(article);
 
 
 
